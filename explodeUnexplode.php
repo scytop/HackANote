1d795953899bbea14ebfa3a676df347ee0da125e
Jonathan Chu <scytop@gmail.com>
2013-11-02 22:00:53 -0700

    Fixed what James rightfully didn't believe in

--- explodeUnexplode.php
+++ explodeUnexplode.php
@@ -1,8 +1,6 @@
-<?
-php
+<?php
 
-function explodeString($rawGroupString, &$groups)
-{
+function explodeString($rawGroupString, &$groups) {
 //figure out how to pull so that you get the string in the format "000000000,000000001,000000002," etc
 $groups = explode(",", $rawGroupString);
 }
@@ -11,16 +9,14 @@ function dickbutt($v1, $v2)
 	return $v1 . "," . $v2;
 }
 function unexplodeString($groupArray)
-{
+{		
+	$toBecomeString = array_reduce($groupArray, "dickbutt");
 	if(strlen($toBecomeString) > 0){
 		//Creates the string in the format "000000000,000000001,000000002,"
-		$toBecomeString = array_reduce($groupArray, "dickbutt");
-		$toBecomeString = $toBecomeString . ".";
+		$toBecomeString = $toBecomeString . ",";
 		$toBecomeString = substr($toBecomeString, 1);
 		}
+		//make sure to set this equal to a variable, e.g. $variable = unexplodeString($groupArray)
 	return $toBecomeString;
 }
-
-
-
 ?>
