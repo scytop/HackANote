bb47d5f6fa2518e5e5a7be2b3d7b29ebc1782fde
Eileen <eileenthe@gmail.com>
2013-11-02 21:02:44 -0700

    commit

--- android/gen/com/hackathon/note/BuildConfig.java
+++ android/gen/com/hackathon/note/BuildConfig.java
@@ -5,4 +5,5 @@ package com.hackathon.note;
 /* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
 public final class BuildConfig {
   public final static boolean DEBUG = Boolean.parseBoolean(null);
+  
 }
